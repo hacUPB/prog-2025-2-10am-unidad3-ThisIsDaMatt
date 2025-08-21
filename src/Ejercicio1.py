31abb8da0aa1afda40398244f73da433892f484f
Matias Munera <matiasmunera08@gmail.com>
2025-08-21 11:05:24 -0500

    Se corrigió un error en el orden de los archivos

--- src/Ejercicio1.py
+++ src/Ejercicio1.py
@@ -1,7 +1,13 @@
-print("Ingrese su nombre y apellido")
-nombre = input()
+nombre = input("Ingrese su nombre y apellido: ")
 #Opcion 2
-print("Bienvenido: ")
-print(nombre)
-#Opcion 2
-print("Bienvenido: ")
+print("Bienvenido: ", nombre)
+#Calcular el IMC de esa persona
+#Leer peso y altura
+peso = input("Ingresa tu peso en kilogramos: ")
+peso = float(peso)
+altura = input("Ingresa tu talla en metros: ")
+altura = float(altura)
+#Calculos
+imc = peso/altura**2
+#Mostrar imc
+print("Tu IMC = ", imc)
