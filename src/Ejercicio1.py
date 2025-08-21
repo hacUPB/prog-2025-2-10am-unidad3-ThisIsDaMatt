3ac272b436ab70d2c161689f1dac454d9331ebd6
Matias Munera <matiasmunera08@gmail.com>
2025-08-21 11:30:51 -0500

    Se completó la primera y segunda actividad

--- src/Ejercicio1.py
+++ src/Ejercicio1.py
@@ -9,5 +9,22 @@ altura = input("Ingresa tu talla en metros: ")
 altura = float(altura)
 #Calculos
 imc = peso/altura**2
+if imc < 18.5:
+    print("Estás debajo de tu peso ideal.")
+else:
+    if imc > 18.5 and imc < 24.9:
+        print("Estás en un peso normal.")
+    else:
+        if imc > 25 and imc < 29.9:
+            print("Estás en sobre peso.")
+        else:
+            if imc > 30 and imc < 34.9:
+                print("Estás en obesidad leve.")
+            else:
+                if imc > 35 and imc < 39.9:
+                    print("Estás en obesidad moderada.")
+                else:
+                    if imc > 40:
+                        print("Estás en obesidad extrema.")
 #Mostrar imc
 print("Tu IMC = ", imc)
---
+++ src/Ejercicio2.py
@@ -0,0 +1,10 @@
+#Determina si un numero es par o impar
+numero = int(input("Ingrese un numero entero: "))
+residuo = numero % 2
+#Si residuo es 0, es par
+if residuo == 0:
+    print(numero, "es par")
+else:
+#Si no, es impar
+    print(numero, "es impar")
+    
