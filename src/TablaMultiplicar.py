634d01b3c5d38a447ddd9e9c1f33475e843206d0
Matias Munera <matiasmunera08@gmail.com>
2025-09-02 11:38:20 -0500

    Se corrigio un error

--- src/TablaMultiplicar.py
+++ src/TablaMultiplicar.py
@@ -5,13 +5,3 @@ while i <= 15:
     resultado = numero * i
     print(f"{numero} x {i} = {resultado}")
     i += 1
-
-'''
-Solución del profesor:
-numero = int(input("Ingrese el numero entero: "))
-cont = 1
-while cont <= 15:
-    res = cont * numero
-    print(f"{numero} x {cont} = {res})
-    
-'''
